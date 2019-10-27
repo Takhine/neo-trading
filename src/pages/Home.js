b8bcca76416d6c28c72d33cf4ee45f8c93571e14
Aniketh <anikethnair97@gmail.com>
2019-10-27 20:29:28 +0530

    Home Banner Complete

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,35 @@
 import React from 'react';
 
+import {
+    Grid,
+    Button
+} from '@material-ui/core';
 
-const Home=()=> {
+import banner from 'static/images/header-desktop.svg';
+import bannerMobile from 'static/images/header-desktop.svg';
+
+const Home = () => {
     return (
-        <div className="home">
-            Home
+        <div className="home container">
+            <Grid container className="home-header">
+                <Grid item xs={12} sm={6}>
+                    <div className="title-container">
+                        <h1 className="main-title">Neo Trading</h1>
+                        <h2 className="slogan">Your Trusted Business Partner</h2>
+                    </div>
+                    <div className="header-buttons-container">
+                            <Button className="products-button">Products</Button>
+                            <Button className="contact-button">Get in touch</Button>
+                    </div>
+                </Grid>
+                <Grid item xs={12} sm={6}>
+                    <div className="banner-container">
+                        <img src={banner} className="desktop" alt="Neo Trading Banner" />
+                        <img src={bannerMobile} className="mobile" alt="Neo Trading Banner" />
+                    </div>
+                </Grid>
+
+            </Grid>
         </div>
     )
 }
